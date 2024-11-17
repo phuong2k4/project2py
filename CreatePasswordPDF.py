d47b56844b38a11ed8ea63c240d358b5c06261bb
phuong2k4 <phuongsepfg@gmail.com>
2024-11-17 10:41:00 -0800

    notification by python create password pdf by python

---
+++ CreatePasswordPDF.py
@@ -0,0 +1,16 @@
+from PyPDF2 import PdfReader, PdfWriter
+from getpass import getpass
+
+def protect_pdf(input_pdf, output_pdf):
+    reader = PdfReader('filelockbypython.pdf')
+    writer = PdfWriter()
+
+    for page in reader.pages:
+        writer.add_page(page)
+
+    password = getpass("Enter your password: ")
+    writer.encrypt(password)
+    with open(output_pdf, "wb") as output_file:
+        writer.write(output_file)
+    print(f"The PDF has password.")
+protect_pdf('filelockbypython.pdf', 'filelockbypython.pdf')
